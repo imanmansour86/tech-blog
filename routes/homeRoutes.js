cf619ad57cc73d61c6be8ca1ceafd5d250f1a08d
imanmansour86 <imanmansour86@gmail.com>
2022-02-08 20:57:55 -0800

    add routes for login and blog

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Blog, User } = require("../models");
+const withAuth = require("../utils/auth");
 
 // GET all blogs
 router.get("/", async (req, res) => {
@@ -8,17 +9,24 @@ router.get("/", async (req, res) => {
       include: [
         {
           model: User,
-          //   attributes: ["name"],
+          attributes: ["name"],
         },
       ],
     });
 
     console.log("dBlogData here", dBlogData);
 
+    // Serialize data so the template can read it
     const blogs = dBlogData.map((blog) => blog.get({ plain: true }));
 
     console.log("blogs here", blogs);
-    res.render("all", { blogs });
+    // res.render("all", { blogs });
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -31,6 +39,7 @@ router.get("/blog/:id", async (req, res) => {
       include: [
         {
           model: User,
+          attributes: ["name"],
         },
       ],
     });
@@ -40,11 +49,45 @@ router.get("/blog/:id", async (req, res) => {
     const blogs = dBlogData.get({ plain: true });
 
     console.log("single here", blogs);
-    res.render("blog", blogs);
+
+    res.render("blog", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get("/blog", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const routes = require("./routes");
 const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -11,7 +12,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 //create handlebars.js engine object
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: "Super secret secret",
