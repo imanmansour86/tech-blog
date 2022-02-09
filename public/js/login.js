69afbeaace50f8df423a5ab8b7c0ac3832e38ad2
imanmansour86 <imanmansour86@gmail.com>
2022-02-08 23:10:15 -0800

    adding signupHandler with post method db

--- public/js/login.js
+++ public/js/login.js
@@ -25,4 +25,28 @@ const loginHandler = async (e) => {
   }
 };
 
+const signupHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("signUpname").value.trim();
+  const email = document.getElementById("signupEmail").value.trim();
+  const password = document.getElementById("signupPassword").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
 document.getElementById("loginForm").addEventListener("submit", loginHandler);
+
+document.getElementById("signUpForm").addEventListener("submit", signupHandler);
